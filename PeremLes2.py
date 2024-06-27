09f20fd9c537f9d10a988f2c5a4697d8e02c6baf
GomzaUlq <UlquiorraSin@yandex.ru>
2024-06-27 15:05:45 +0500

    Add files via upload

---
+++ PeremLes2.py
@@ -0,0 +1,5 @@
+Homeworks_done = '12'
+Hour_for_homeworks = '1.5'
+Name_of_course = 'Python'
+Time_for_lesson = float(Hour_for_homeworks) / float(Homeworks_done)
+print(Name_of_course ,Homeworks_done ,Hour_for_homeworks , Time_for_lesson)
