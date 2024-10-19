8805d0c30b70774c4ebb58dd662fd0dbcf1edc8a
GomzaUlq <UlquiorraSin@yandex.ru>
2024-10-19 18:57:40 +0500

    Add files via upload

---
+++ Tests.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+import Runar
+
+
+class RunnerTest(TestCase):
+    def test_walk(self):
+        run_1 = Runar.Runner('Бегун_1')
+        for i in range(10):
+            run_1.walk()
+        self.assertEqual(run_1.distance, 50)
+
+    def test_run(self):
+        run_2 = Runar.Runner('Бегун_2')
+        for i in range(10):
+            run_2.run()
+        self.assertEqual(run_2.distance, 100)
+
+    def test_challenge(self):
+        run_3 = Runar.Runner('Бегун_3')
+        run_4 = Runar.Runner('Бегун_4')
+        for i in range(10):
+            run_3.walk()
+            run_4.run()
+        self.assertNotEqual(run_3.distance, run_4.distance)
