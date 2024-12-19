c99d1a10e07522c6c53636ef5d201d1016a76332
GomzaUlq <UlquiorraSin@yandex.ru>
2024-12-19 14:04:45 +0500

    Add files via upload

---
+++ 16.5.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI, Path, HTTPException, Body, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from typing import Annotated, List
+from pydantic import BaseModel
+
+app = FastAPI()
+templates = Jinja2Templates(directory="temlpates")
+users = []
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_user(request: Request):
+    return templates.TemplateResponse("users.html", {"request": request, "user": users})
+
+
+@app.get('/user/{user_id}', response_class=HTMLResponse)
+async def get_users(request: Request, user_id: Annotated[int, Path(ge=1)]):
+    return templates.TemplateResponse("users.html", {"request": request, "users": user_id})
+
+
+@app.post("/user/{username}/{age}")
+async def create_user(username: Annotated[str, Path(min_length=5,
+                                                    max_length=20,
+                                                    description="Enter username",
+                                                    example="UrbanUser")],
+                      age: Annotated[int, Path(ge=18,
+                                               le=120,
+                                               description="Enter age",
+                                               example=25)]):
+    user_id = max((user.id for user in users), default=0) + 1
+    new_user = User(id=user_id, username=username, age=age)
+    users.append(new_user)
+    return new_user
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: Annotated[int, Path(gt=0)],
+                      username: Annotated[str, Path(min_length=5,
+                                                    max_length=20,
+                                                    description="Enter username",
+                                                    example="UrbanUser")],
+                      age: Annotated[int, Path(ge=18,
+                                               le=120,
+                                               description="Enter age",
+                                               example=25)]):
+    try:
+        users[user_id - 1] = User(id=user_id, username=username, age=age)
+        return users[user_id - 1]
+    except IndexError:
+        raise HTTPException(status_code=404, detail='User was not found')
+
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: int) -> str:
+    try:
+        users.pop(user_id - 1)
+        return users.pop(user_id - 1)
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
